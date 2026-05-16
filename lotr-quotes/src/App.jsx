80035bd17a4459105c16cf56308b9fdf3b28431d
Johnny Rosenquist <johnny.r@live.se>
2026-05-16 19:14:23 +0200

    feat: implement api functionality and creation of questions

--- lotr-quotes/src/App.jsx
+++ lotr-quotes/src/App.jsx
@@ -3,21 +3,29 @@ import QuestionCard from "./components/QuestionCard/QuestionCard";
 import Stack from "react-bootstrap/Stack";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { fetchRandomQuote, getAnswerAlternatives } from "./api/lotrapi";
+import { useState, useEffect } from "react";
 
-const placeholderQuestion = {
-  quote:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-  alternatives: ["Boromir", "Aragorn", "Frodo", "Gandalf"],
-};
 const placeholderScore = { score: "1", questionsAnswered: "5" };
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(undefined);
+
+  async function loadQuestion() {
+    const currQuote = await fetchRandomQuote();
+    const currAlternatives = await getAnswerAlternatives(currQuote.characterId);
+    setCurrentQuestion({
+      quote: { phrase: currQuote.quote, character: currQuote.characterId },
+      alternatives: currAlternatives,
+    });
+  }
+
   return (
     <Container>
       <Stack>
         <h1>LOTR - Who said it?</h1>
         <ScoreCard score={placeholderScore} />
-        <QuestionCard question={placeholderQuestion} />
+        <QuestionCard question={currentQuestion} />
         <Button>Restart</Button>
       </Stack>
     </Container>
