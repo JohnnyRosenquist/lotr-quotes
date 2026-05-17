4bedc99cf1ff334920dfea87cff126f86a2d4cda
Johnny Rosenquist <johnny.r@live.se>
2026-05-17 17:16:54 +0200

    feat: Add floating UI to display correct answer in popup

--- lotr-quotes/src/components/SubmitButton/SubmitButton.jsx
+++ lotr-quotes/src/components/SubmitButton/SubmitButton.jsx
@@ -2,7 +2,12 @@ import Button from "react-bootstrap/Button";
 
 export default function SubmitButton({ grade, disabled }) {
   return (
-      <Button disabled={disabled} className="mt-2 mb-2" variant="success" onClick={grade}>
+    <Button
+      disabled={disabled}
+      className="mt-2 mb-2"
+      variant="success"
+      onClick={grade}
+    >
       Submit answer
     </Button>
   );
